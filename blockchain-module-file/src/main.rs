442be0cd4ac301d48a447cde5a678d152f57ba7e
Coding迪斯尼 <yichen@real.com>
2024-08-13 10:59:41 +0800

    Add files via upload

---
+++ blockchain-module-file/src/main.rs
@@ -0,0 +1,21 @@
+pub mod blockchain;
+use blockchain::BlockChain;
+
+fn main() {
+    // //convert a string to bytes array
+    // //convert it to String, into_bytes() => Vec<u8>
+    // let b = Block::new(0, "this is our first block!".to_string().into_bytes());
+    // b.print();
+
+    // println!("the first block is : {:?}", b);
+
+    let mut block_chain = BlockChain::new();
+    println!("Block chain:{:?}", block_chain);
+
+    block_chain.print();
+
+    block_chain.create_block(1, "hash 1".to_string().into_bytes());
+    block_chain.print();
+    block_chain.create_block(2, "hash 2".to_string().into_bytes());
+    block_chain.print();
+}
